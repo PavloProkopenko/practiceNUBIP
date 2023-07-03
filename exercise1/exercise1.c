1521e6f241c0144c0c5c704ae8aa4ca3817c2696
Pavlo Prokopenko <stpasha6@gmail.com>
2023-07-03 13:56:00 +0300

    Added comments

--- exercise1/exercise1.c
+++ exercise1/exercise1.c
@@ -7,34 +7,34 @@ int main()
     printf("Input 3 values:\n"); // Просимо користувача ввести три значення часу
     scanf("%d%d%d", &t1, &t2, &t3); // Зчитування значень з клавіатури
 
-    if (t1 <= 0 || t1 > 10000)
+    if (t1 <= 0 || t1 > 10000) // Перевірка значення t1
     {
         printf("Invalid input! Time values must be between 1 and 10000.\n");
 
         do {
             printf("Input value for t1: ");
             scanf("%d", &t1);
-        } while (t1 <= 0 || t1 > 10000);
+        } while (t1 <= 0 || t1 > 10000); // Код виконується до того моменту, доки значення не буде вірним
     }
 
-    if (t2 <= 0 || t2 > 10000)
+    if (t2 <= 0 || t2 > 10000) // Перевірка значення t2
     {
         printf("Invalid input! Time values must be between 1 and 10000.\n");
 
         do {
             printf("Input value for t2: ");
             scanf("%d", &t2);
-        } while (t2 <= 0 || t2 > 10000);
+        } while (t2 <= 0 || t2 > 10000); // Код виконується до того моменту, доки значення не буде вірним
     }
     
-    if (t3 <= 0 || t3 > 10000)
+    if (t3 <= 0 || t3 > 10000) // Перевірка значення t3
     {
         printf("Invalid input! Time values must be between 1 and 10000.\n");
 
         do {
             printf("Input value for t3: ");
             scanf("%d", &t3);
-        } while (t3 <= 0 || t3 > 10000);
+        } while (t3 <= 0 || t3 > 10000); // Код виконується до того моменту, доки значення не буде вірним
     }
     
     float total_time = (t1 + t2 + t3) / 3; // Переведення значень в години за формолую, вказаною в теоретичних відомостях
