dc3135b91c899043be664b11fecbec087bb1bab0
Pavlo Prokopenko <stpasha6@gmail.com>
2023-07-10 12:42:06 +0300

    Added comments

--- exercise9/exercise9.c
+++ exercise9/exercise9.c
@@ -5,11 +5,11 @@ int minimum_steps(unsigned long x, unsigned long y);
 
 int main()
 {
-    unsigned long x, y;
+    unsigned long x, y; // Оголошення змінних
 
-    printf("Enter the value of x: ");
-    scanf("%lu", &x);
-    while (x < 0 || x > pow(2, 31))
+    printf("Enter the value of x: "); // Підказка для користувача, що треба ввести значення Х
+    scanf("%lu", &x); // Зчитування з клавіатури
+    while (x < 0 || x > pow(2, 31)) // Перевірка, чи задовільняє значення Х умову задачі
     {
         printf("!!!! Invalid value for x !!!!\n*Value must be greater or equal 0 and less than 2^31*\n");
         printf("Enter the value of x: ");
@@ -17,29 +17,29 @@ int main()
     }
     
 
-    printf("Enter the value of y: ");
-    scanf("%lu", &y);
-    while (y < 0 || y > pow(2, 31))
+    printf("Enter the value of y: "); // Підказка для користувача, що треба ввести значення Y
+    scanf("%lu", &y); // Зчитування з клавіатури
+    while (y < 0 || y > pow(2, 31)) // Перевірка, чи задовільняє значення Y умову задачі
     {
         printf("!!!! Invalid value for y !!!!\n*Value must be greater or equal 0 and less than 2^31*\n");
         printf("Enter the value of y: ");
         scanf("%lu", &y);
     }
 
-    if (y < x)
+    if (y < x) // Якщо значення Х більше ніж значення Y, то програма завершить роботу
     {
         printf("Value Y must be greater than value X");
         return -1;
     }
     
-    int steps = minimum_steps(x, y);
+    int steps = minimum_steps(x, y); // Створення змінної, в яку буде записано значення мінімально потрібних кроків між Х та Y
 
-    printf("Minimum steps: %d\n", steps);
+    printf("Minimum steps: %d\n", steps); // Виведення резульату на екран
 
     return 0;
 }
 
-int minimum_steps(unsigned long x, unsigned long y)
+int minimum_steps(unsigned long x, unsigned long y) // Реалізація функції (формули взяті з відкритих джерел)
 {
     unsigned long diff = y - x;
     unsigned long root = (unsigned long)sqrt(1 + 8*diff);
@@ -53,3 +53,4 @@ int minimum_steps(unsigned long x, unsigned long y)
         return base + 2;
     }
 }
+
